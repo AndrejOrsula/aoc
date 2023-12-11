36e7a93dbe70de5c024bbf404f265e35f6649ea2
Andrej Orsula <orsula.andrej@gmail.com>
2023-12-11 22:50:02 +0100

    aoc2023/day9: Refactor code
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

--- aoc2023/src/day9.rs
+++ aoc2023/src/day9.rs
@@ -1,7 +1,7 @@
 use aoc_runner_derive::{aoc, aoc_generator};
 
 #[aoc_generator(day9)]
-fn parse(input: &str) -> utils::ParsedInput {
+fn parse(input: &str) -> Vec<Vec<i32>> {
     input
         .lines()
         .map(|line| {
@@ -12,9 +12,28 @@ fn parse(input: &str) -> utils::ParsedInput {
         .collect()
 }
 
-mod utils {
-    pub type ParsedInput = Vec<Vec<i32>>;
+#[aoc(day9, part1)]
+fn part1(input: &[Vec<i32>]) -> i32 {
+    input
+        .iter()
+        .map(|seq| utils::predict_next_recursively(seq))
+        .sum()
+}
 
+#[aoc(day9, part2)]
+fn part2(input: &[Vec<i32>]) -> i32 {
+    input
+        .to_owned()
+        .iter_mut()
+        .map(|arr| {
+            arr.reverse();
+            arr
+        })
+        .map(|seq| utils::predict_next_recursively(seq))
+        .sum()
+}
+
+mod utils {
     pub fn predict_next_recursively(row: &[i32]) -> i32 {
         let next_row: Vec<_> = row.windows(2).map(|w| w[1] - w[0]).collect();
 
@@ -25,27 +44,6 @@ mod utils {
                 .then(|| predict_next_recursively(&next_row))
                 .unwrap_or(0)
     }
-}
-
-#[aoc(day9, part1)]
-fn part1(input: &utils::ParsedInput) -> i32 {
-    input
-        .iter()
-        .map(|seq| utils::predict_next_recursively(seq))
-        .sum()
-}
-
-#[aoc(day9, part2)]
-fn part2(input: &utils::ParsedInput) -> i32 {
-    input
-        .clone()
-        .into_iter()
-        .map(|mut arr| {
-            arr.reverse();
-            arr
-        })
-        .map(|seq| utils::predict_next_recursively(&seq))
-        .sum()
 }
 
 #[cfg(test)]
